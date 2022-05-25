e7f16e890e7578865ecbe0806a6807da254cc536
UniversalOverlord <52113114+UniversalOverlord@users.noreply.github.com>
2022-05-25 14:28:00 +0100

    Create DatabaseConfig.php

---
+++ PHP/DatabaseConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+class DataBaseConfig
+{
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+
+        $this->servername = 'e3fruits.space';
+        $this->username = 'ethrfrui_WPDVW';
+        $this->password = 'new98765';
+        $this->databasename = 'ethrfrui_WPDVW';
+
+    }
+}
+
+?>
